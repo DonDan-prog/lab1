f27d720f4f58ff2f4360bdc445bb1587ae427954
Daniil Ilichev <ilichevdaniil@gmail.com>
2021-02-11 14:27:34 +0300

    added reverseString func

--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,5 @@
+import jdk.internal.joptsimple.internal.Strings;
+
 public class Palindrome 
 {
     public static void main(String[] args)
@@ -5,7 +7,14 @@ public class Palindrome
         for(int i = 0; i < args.length; i++)
         {
             String s = args[i];
-            
+
         }
-    }    
+    }
+    public static String reverseString(String s)
+    {
+        String ret = "";
+        for(int i = s.length() - 1; i >= 0; --i)
+            ret += s.charAt(i);
+        return ret;
+    }
 }

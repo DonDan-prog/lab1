807d01998fc28a9cf4ef4ab3b714ccd98b5d7a3b
Daniil Ilichev <ilichevdaniil@gmail.com>
2021-02-11 14:17:35 +0300

    Added func makePrimes

--- Primes.java
+++ Primes.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+
 public class Primes
 {
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
         System.out.println(isPrime(7));
+        System.out.println(makePrimes());
     }
     public static boolean isPrime(int n)
     {
@@ -13,5 +16,15 @@ public class Primes
                 return false;
         }
         return true;
+    }
+    public static ArrayList<Integer> makePrimes()
+    {
+        ArrayList<Integer> primes = new ArrayList<>();
+        for(int i = 1; i < 100; ++i)
+        {
+            if(isPrime(i))
+                primes.add(i);
+        }
+        return primes;
     }
 }
